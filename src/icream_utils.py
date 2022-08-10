8f1aac3c4be8b1ac736426a4472983802f91b43d
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-10 14:53:29 +0200

    [src-utils] adding tag parsers.

--- src/icream_utils.py
+++ src/icream_utils.py
@@ -1,58 +1,22 @@
-from random import randint
-
-# based on:
-# https://www.geeksforgeeks.org/python-get-unique-values-list/
-# function to get unique values
-def unique1(non_unique_list):
- 
-    # initialize a null list
-    unique_list = []
-     
-    # traverse for all elements
-    for x in non_unique_list:
-        # check if exists in unique_list or not
-        if x not in unique_list:
-            unique_list.append(x)
-    
-    return unique_list
-
-
-# based on:
-# https://stackoverflow.com/questions/12897374/get-unique-values-from-a-list-in-python
-# function to get unique values
-def unique2(non_unique_list):
-    return list(set(non_unique_list))
-
-# based on:
-# https://stackoverflow.com/questions/12897374/get-unique-values-from-a-list-in-python
-# function to get unique values
-def unique3(non_unique_list):
-
-    _set = set()
-     
-    # traverse for all elements
-    for x in non_unique_list:
-        _set.add(x)     
-    return list(_set)
 
 # function to get unique values
 # '-> based on: https://stackoverflow.com/questions/12897374/get-unique-values-from-a-list-in-python
 # '-> NOTE: fastest from considered options
-def unique(non_unique_list):
+def unique(non_unique_list : list):
     return list(set(non_unique_list))
 
-if __name__=="__main__":
-    import time
+# parse string into categories in format:
+# <some text> "[" <some tag1> "-" <some tag2> "-" ... "]" <some text>
+def parse_tags(s : str, _left_separator = "[", _right_separator = "]", _middle_separator = "-"):
+    # splitting to separated levels
+    _to_parse = s.split(_right_separator)[0].split(_left_separator)[1].split(_middle_separator)
+    # parsing and modifying tags
+    _tags = [" ".join(_ss.strip().lower().capitalize().split()) for _ss in _to_parse]
+    return _tags
 
-    _l = []
-    for i in range(1000):
-        _l.append(randint(0,100))
-    
-    uniques = [unique1,unique2,unique3]
-    for i in range(3):
-        unique = uniques[i]
-        _start = time.time()
-        a = unique(_l)
-        _end = time.time()
-        _elapsed = _end - _start
-        print("fcn {} elapsed: {}".format(i, _elapsed))
+if __name__=="__main__":
+
+    ws = ["[a-borka a borec-c]", " a dafda fda [ s st - - gfa    fda  fda fad    f. ] dafa fda"]
+
+    for w in ws:
+        print("{} -> {}".format(w,parse_tags(w)))
