32e27480e612a285ca0fe9d164c65ea18ee3f5c8
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-11 14:50:01 +0200

    [src-calendar] adding calendar that can be loaded from ics file and printed.

---
+++ src/icreamcalendar.py
@@ -0,0 +1,38 @@
+from icreamevent import ICREAMEvent
+ 
+from copy import deepcopy
+
+# icalendar interface
+from icalendar import Calendar, Event
+
+class ICREAMCalendar:
+
+    def __init__(self, path = None, calendar=None):
+        self.events = list()
+        if path is not None:
+            self.load_calendar(path)
+        if calendar is not None:
+            self.events = deepcopy(calendar.events)
+
+    # load calendar from file
+    def load_calendar(self, path):
+        with open(path, "r") as f:
+            # read calendar from file
+            ical = Calendar.from_ical(f.read())
+            f.close()
+        
+            for component in ical.walk():
+                #print(component.name)
+                if component.name == "VEVENT":
+                    icrm_ev = ICREAMEvent(component=component)
+                    self.events.append(icrm_ev)
+    
+    # prints callendar
+    def print_calendar(self):
+        for ev in self.events:
+            print(ev)
+
+if __name__=="__main__":
+    _path = "./cals/kubikji2@fel.cvut.cz.ics"
+    icrm_cal = ICREAMCalendar(path=_path)
+    icrm_cal.print_calendar()
