68b93430e44eea7dadf07d937601a6828e465bc6
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-11 14:48:31 +0200

    [src-event] adding icalendar.component parsing.

--- src/icreamevent.py
+++ src/icreamevent.py
@@ -1,15 +1,42 @@
+from icalendar import Event
 
+from icream_utils import *
+
+# basic event object used by ICREAMCalendar
 class ICREAMEvent:
-    
-    def ICREAMEvent(self, start=None, end=None, name="", organizer="", description=""):
+
+    # constructor
+    # '-> either use particular data, or use icalendar.Event to initialize
+    def __init__(self, start=None, end=None, name="", organizer="", description="", component=None):
         self.start = start
         self.end = end
         self.name = name
         self.organizer = organizer
         self.description = description
-        pass
+        if component is not None:
+            self.parse_component(component=component)
+    
+    # update ICREAMEvent using icalendar.Event
+    def parse_component(self, component : Event):
+        if component.name == "VEVENT":
+            self.name = replace_mult_spaces(component.get('summary'))
+            # decoding based on: https://icalendar.readthedocs.io/en/latest/usage.html#more-documentation
+            self.start = icream_date_converter(component.decoded('dtstart'))
+            self.end = icream_date_converter(component.decoded('dtend'))
+            self.organizer = iceam_parse_email(component.get('organizer'))
+            self.description = component.get('description')
+            print(self.name)
+        else:
+            print("[ICREAM-event] invalid component {}=!=\'VEVENT\'".format(component.name))
 
+    # computes event durartion
     def get_duration(self):
         return 0 if (self.start is None) or (self.end is None) else self.end - self.start
-    
+
+    # string representatoni
+    def __str__(self):
+        dur_s = icream_duration_formater(self.start, self.end)
+        s = "{} ({}) by {}".format(self.name, dur_s, self.organizer)
+        return s
+
     
